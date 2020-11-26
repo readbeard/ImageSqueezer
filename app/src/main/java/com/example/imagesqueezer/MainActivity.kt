e1a4cd3c645c5ecf713d370092a1a7fee7f4b25a
readbeard <matteo.principe92@gmail.com>
2020-11-26 12:13:54 +0100

    initial commit
    This commit is the very first one. The project has been initialized and
    sketched, but some choices may be changed (e.g.: the used libraries).
    Moreover, code looks pretty bad for now, but we-re in a rush
    implementing features in order to test them ASAP.

---
+++ app/src/main/java/com/example/imagesqueezer/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.example.imagesqueezer
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.kbeanie.multipicker.api.ImagePicker
+import com.kbeanie.multipicker.api.Picker
+import com.kbeanie.multipicker.api.callbacks.ImagePickerCallback
+import com.kbeanie.multipicker.api.entity.ChosenImage
+import com.kbeanie.multipicker.core.ImagePickerImpl
+import com.kbeanie.multipicker.utils.LogUtils
+
+
+class MainActivity : AppCompatActivity(), ImagePickerCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback {
+    private lateinit var imagePicker: ImagePicker
+    private var lvResults: ListView? = null
+    private val requiredPermissions: Array<String?>
+        get() = try {
+            val info = this.packageManager
+                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            if (ps != null && ps.isNotEmpty()) {
+                ps
+            } else {
+                arrayOfNulls(0)
+            }
+        } catch (e: Exception) {
+            arrayOfNulls(0)
+        }
+
+    private val runtimePermissions: Unit
+        get() {
+            val allNeededPermissions: MutableList<String?> = ArrayList()
+            for (permission in requiredPermissions) {
+                if (!isPermissionGranted(this, permission)) {
+                    allNeededPermissions.add(permission)
+                }
+            }
+            if (allNeededPermissions.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    allNeededPermissions.toTypedArray(),
+                    PERMISSION_REQUESTS
+                )
+            }
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        initializeImagePicker()
+
+        findViewById<Button>(R.id.button_contentmain_pick).setOnClickListener {
+            imagePicker.pickImage()
+        }
+
+        lvResults = findViewById<ListView>(R.id.lvResults);
+
+        if (!allPermissionsGranted()) {
+            runtimePermissions
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in requiredPermissions) {
+            if (!isPermissionGranted(this, permission)) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    private fun initializeImagePicker() {
+        imagePicker = ImagePicker(this);
+        imagePicker.allowMultiple();
+        imagePicker.setImagePickerCallback(this);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Picker.PICK_IMAGE_DEVICE) {
+                if (imagePicker == null) {
+                    initializeImagePicker()
+                }
+
+                /*
+                var compressedFile: File
+                var compressedImageFile: File
+                val fileCreation = lifecycleOwner.lifecycleScope.async(Dispatchers.IO) {
+                    compressedFile = File(file.originalPath + "_compressed")
+                    if (!compressedFile.exists()) {
+                        try {
+                            compressedFile.createNewFile()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    lifecycleOwner.lifecycleScope.async(Dispatchers.IO) {
+                        compressedImageFile = Compressor.compress(context, compressedFile)
+
+                    }
+                }
+*/
+                imagePicker.submit(data)
+            }
+        }
+    }
+
+    override fun onError(p0: String?) {
+    }
+
+    override fun onImagesChosen(images: List<ChosenImage>) {
+        val adapter = MediaResultsAdapter(images, this)
+        adapter.lifecycleOwner = this
+        lvResults?.adapter = adapter
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "Permission granted!")
+        if (allPermissionsGranted()) {
+            //TODO implement
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.simpleName
+        private const val PERMISSION_REQUESTS = 1
+
+        private fun isPermissionGranted(
+            context: Context,
+            permission: String?
+        ): Boolean {
+            if (ContextCompat.checkSelfPermission(context, permission!!)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.i(TAG, "Permission granted: $permission")
+                return true
+            }
+            Log.i(TAG, "Permission NOT granted: $permission")
+            return false
+        }
+    }
+}
---
+++ app/src/main/java/com/example/imagesqueezer/MediaResultsAdapter.kt
@@ -0,0 +1,130 @@
+package com.example.imagesqueezer
+
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.kbeanie.multipicker.api.entity.ChosenFile
+import com.kbeanie.multipicker.api.entity.ChosenImage
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.default
+import id.zelory.compressor.constraint.destination
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import java.io.File
+
+@GlideModule
+class MediaResultsAdapter(private val files: List<ChosenFile>, private val context: Context) :
+    BaseAdapter() {
+
+    lateinit var lifecycleOwner: LifecycleOwner
+
+    override fun getCount(): Int {
+        return files.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return files[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var mConvertView = convertView
+        Log.d(TAG, "getView: " + files.size)
+        val file = getItem(position) as ChosenFile
+        val itemViewType = getItemViewType(position)
+        if (mConvertView == null) {
+            when (itemViewType) {
+                TYPE_IMAGE -> mConvertView = LayoutInflater.from(
+                    context
+                ).inflate(R.layout.img_adapter, null)
+                TYPE_FILE -> mConvertView = LayoutInflater.from(
+                    context
+                ).inflate(R.layout.file_adapter, null)
+            }
+        }
+        when (itemViewType) {
+            TYPE_IMAGE -> showImage(file, mConvertView!!)
+        }
+        return mConvertView!!
+    }
+
+
+    private fun showImage(file: ChosenFile, view: View) {
+        val image = file as ChosenImage
+        val tvName = view.findViewById<View>(R.id.tvName) as TextView
+        tvName.text = file.getDisplayName()
+        val tvCompleteMimeType = view.findViewById<View>(R.id.tvCompleteMimeType) as TextView
+        tvCompleteMimeType.text = file.getMimeType()
+        val ivImage = view.findViewById<View>(R.id.ivImage) as ImageView
+
+        if (image.thumbnailSmallPath != null) {
+            Glide.with(context).load(Uri.fromFile(File(image.thumbnailSmallPath))).into(ivImage)
+        }
+        val tvDimension = view.findViewById<View>(R.id.tvDimension) as TextView
+        tvDimension.text = String.format(FORMAT_IMAGE_VIDEO_DIMENSIONS, image.width, image.height)
+        val tvMimeType = view.findViewById<View>(R.id.tvMimeType) as TextView
+        tvMimeType.text = file.getFileExtensionFromMimeTypeWithoutDot()
+        val tvSize = view.findViewById<View>(R.id.tvSize) as TextView
+        tvSize.text = file.getHumanReadableSize(false)
+        val tvOrientation = view.findViewById<View>(R.id.tvOrientation) as TextView
+        tvOrientation.text =
+            String.format(FORMAT_ORIENTATION, image.orientationName)
+
+
+
+        view.setOnClickListener {
+            Log.i(
+                TAG,
+                "onClick: Tapped: " + image.originalPath
+            )
+
+            runBlocking {
+                lifecycleOwner.lifecycleScope.async(Dispatchers.Main) {
+                    Compressor.compress(context, File(file.originalPath)) {
+                        default()
+                        destination(File(file.originalPath.substring(0, file.originalPath.indexOf(".jpg")) + "-compressed.jpg"))
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val type = (getItem(position) as ChosenFile).type
+        when (type) {
+            "image" -> return TYPE_IMAGE
+            "file" -> return TYPE_FILE
+        }
+        return TYPE_FILE
+    }
+
+    companion object {
+        private val TAG = MediaResultsAdapter::class.java.simpleName
+        private const val TYPE_IMAGE = 0
+        private const val TYPE_FILE = 2
+        private const val FORMAT_IMAGE_VIDEO_DIMENSIONS = "%sw x %sh"
+        private const val FORMAT_ORIENTATION = "Ortn: %s"
+    }
+}
